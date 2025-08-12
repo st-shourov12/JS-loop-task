a682cbc924aa363fe0b6fd297a65818eb9cb92d9
st-shourov12 <shourovsc38@gmail.com>
2025-08-12 23:30:29 +0600

    Solving For loop task 2

--- for/1 commitment.js
+++ for/1 commitment.js
@@ -7,4 +7,4 @@
 for (let x = 0; x < 60; x++) {
     console.log("I will invest at least 6 hrs every single day for next 60 days!");
     
-}
+};
--- for/2 odd even.js
+++ for/2 odd even.js
@@ -6,6 +6,13 @@ Find all the odd numbers from 61 to 100.
 
  */
 
+for (let v = 61; v <= 100; v++) {
+    if (v % 2 !== 0) {
+        console.log("odd number ",v);
+    }
+    
+}
+
 
 /***
 
@@ -15,4 +22,9 @@ Find all the even numbers from 78 to 98.
 
  */
 
-/*programming hero*/
+for (let x = 78; x <= 98; x++) {
+    if (x % 2 !== 0) {
+        continue;
+    }
+    console.log("even number is ", x);
+}
