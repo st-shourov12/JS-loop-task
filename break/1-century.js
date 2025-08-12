56781ddd9dc561a8dfb740b2342b791a9c423ba4
st-shourov12 <shourovsc38@gmail.com>
2025-08-12 22:37:15 +0600

    Solving sum related break

--- break/1-century.js
+++ break/1-century.js
@@ -5,9 +5,10 @@ Write a loop 1 to 200. Use break to exit the loop once you find 100.
 */
 
 for (let index = 1; index <= 200; index++) {
+    console.log(index);
     if(index >= 100){
         break
     }
-    console.log(index);
+    
     
 }
