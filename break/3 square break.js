78068ddb27f7d24b820f503d4c67e14820a4c75e
st-shourov12 <shourovsc38@gmail.com>
2025-08-12 22:58:37 +0600

    Solving square root related break

--- break/3 square break.js
+++ break/3 square break.js
@@ -1,3 +1,15 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+
+for (let n = 1; n <= 100; n++) {
+    console.log(n);
+
+    
+    if (Number.isInteger(Math.sqrt(n)) && n !== 1 ) {
+        console.log(`The First Square number is: ${n}`);
+        break
+    }
+    
+}
