31feab344f5f2a1550097c870f167cc93aef3419
st-shourov12 <shourovsc38@gmail.com>
2025-08-12 23:18:57 +0600

    Using continue to see odd number

--- continue/2 skip-five.js
+++ continue/2 skip-five.js
@@ -1,3 +1,12 @@
 /*
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
+let n = 55;
+while (n <= 85) {
+    n++;
+    if (n % 2 === 0 || n % 5 === 0) {
+        continue;
+    }
+    console.log(n);
+    
+}
