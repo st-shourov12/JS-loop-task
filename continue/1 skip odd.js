0a8102f22cfdb188f09bc1eb3279a84cd2e1517d
st-shourov12 <shourovsc38@gmail.com>
2025-08-12 23:10:14 +0600

    Using continue

--- continue/1 skip odd.js
+++ continue/1 skip odd.js
@@ -1,3 +1,15 @@
 /* 
 Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
 */
+
+let n = 0;
+
+while (n <= 40) {
+
+    n++ ;
+    if (n % 2 !== 0) {
+        continue
+    }
+    
+  console.log(n);
+}
