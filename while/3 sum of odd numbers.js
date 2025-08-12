1ced604f3ca6ca73660d323e771a4d738841fc00
st-shourov12 <shourovsc38@gmail.com>
2025-08-13 01:01:08 +0600

    Solving while  loop task 3

--- while/3 sum of odd numbers.js
+++ while/3 sum of odd numbers.js
@@ -7,6 +7,24 @@ Subtask-1:
 Display sum of all the odd numbers from 81 to 131.
 
  */
+let sum = 0;
+let i = 81;
+
+while (i <= 131) {
+    if (i % 2 !== 0){
+        sum += i ;
+        console.log("The current number is ", i ," Sum of the odd number ",sum);
+    }
+    i++;
+}
+
+
+
+
+
+
+
+
 /***
 
 Subtask-2:
@@ -15,4 +33,13 @@ Display sum of all the even numbers from 206 to 311.
 
  */
 
-/*programming hero*/
+let summation = 0;
+let e = 206; 
+
+while (e <= 311) {
+    if (e % 2 === 0) {
+        summation += e;
+        console.log(e, "The even number summation is ",summation)
+    }
+    e++;
+}
